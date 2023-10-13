11150880b26b09e6541229c37ceaf3a996bdff91
Issouf <issoufouedraogo924@gmail.com>
2023-10-13 19:49:45 +0000

    Dernier commit du vendredi soir

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -80,59 +80,59 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function storePersonneMorale(Request $request): RedirectResponse
-    {
-        $request->validate([
-            // 'id' => ['required'],
-            'name' => ['required', 'string', 'max:255'],
-            'ifu' => ['required', 'string', 'max:255'],
-            'rccm' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
-            'telephone' => ['required', 'String', 'max:255'],
-            'siege_social' => ['required', 'string', 'max:255'],
-            'boite_postale' => ['required', 'string', 'max:255'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
-        // dd($request);
+    // public function storePersonneMorale(Request $request): RedirectResponse
+    // {
+    //     $request->validate([
+    //         // 'id' => ['required'],
+    //         'name' => ['required', 'string', 'max:255'],
+    //         'ifu' => ['required', 'string', 'max:255'],
+    //         'rccm' => ['required', 'string', 'max:255'],
+    //         'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+    //         'telephone' => ['required', 'String', 'max:255'],
+    //         'siege_social' => ['required', 'string', 'max:255'],
+    //         'boite_postale' => ['required', 'string', 'max:255'],
+    //         'password' => ['required', 'confirmed', Rules\Password::defaults()],
+    //     ]);
+    //     // dd($request);
 
-        $user = User::create([
-            //'id' => Str::uuid(), // Génération d'un UUID pour l'utilisateur
-            'name' => $request->email,
-            //'prenom' => $request->prenom,
-            //'telephone' => $request->telephone,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+    //     $user = User::create([
+    //         //'id' => Str::uuid(), // Génération d'un UUID pour l'utilisateur
+    //         'name' => $request->email,
+    //         //'prenom' => $request->prenom,
+    //         //'telephone' => $request->telephone,
+    //         'email' => $request->email,
+    //         'password' => Hash::make($request->password),
+    //     ]);
 
-        $usager = Usager::create([
-            //'id' => Str::uuid(), // Génération d'un UUID pour l'utilisateur
-            'nom_entreprise' => $request->name,
-            'ifu' => $request->ifu,
-            'rccm' => $request->rccm,
-            'siege_social' => $request->siege_social,
-            'boite_postale' => $request->boite_postale,
-            'telephone' => $request->telephone,
-            'password' => Hash::make($request->password),
-        ]);
+    //     $usager = Usager::create([
+    //         //'id' => Str::uuid(), // Génération d'un UUID pour l'utilisateur
+    //         'nom_entreprise' => $request->name,
+    //         'ifu' => $request->ifu,
+    //         'rccm' => $request->rccm,
+    //         'siege_social' => $request->siege_social,
+    //         'boite_postale' => $request->boite_postale,
+    //         'telephone' => $request->telephone,
+    //         'password' => Hash::make($request->password),
+    //     ]);
 
-        //$user->usager_id = $usager->uuid;
+    //     //$user->usager_id = $usager->uuid;
 
-        // dd($user);
+    //     // dd($user);
 
-        event(new Registered($user));
-        event(new Registered($usager));
+    //     event(new Registered($user));
+    //     event(new Registered($usager));
 
-        $user->usager_id = $usager->uuid;
-        $usager->type_usager_id = TypeUsager::where('libelle_long', '=', 'Personne morale')->first()->uuid;
-        $user->save();
-        $usager->save();
+    //     $user->usager_id = $usager->uuid;
+    //     $usager->type_usager_id = TypeUsager::where('libelle_long', '=', 'Personne morale')->first()->uuid;
+    //     $user->save();
+    //     $usager->save();
 
-        Auth::login($user);
+    //     Auth::login($user);
 
-        // session()->flash('success', 'Registration successful!');
+    //     // session()->flash('success', 'Registration successful!');
 
-        return redirect(RouteServiceProvider::HOME)->with('success', 'Bienvenue au Formulaire de demandes d\'avis technique d\'importation de produits chimique !');;
-    }
+    //     return redirect(RouteServiceProvider::HOME)->with('success', 'Bienvenue au Formulaire de demandes d\'avis technique d\'importation de produits chimique !');;
+    // }
 
     /**
      * Handle an incoming registration request.
@@ -172,7 +172,7 @@ class RegisteredUserController extends Controller
             'boite_postale' => $request->boite_postale,
             'telephone' => $request->telephone,
             'password' => Hash::make($request->password),
-        ]); 
+        ]);
 
         $user->usager_id = $usager->uuid;
 
